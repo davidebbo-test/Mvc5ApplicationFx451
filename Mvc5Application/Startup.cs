7e0d3231020e8d56f6aaae30998dc30f297ea4bf
davidebbo <david.ebbo@microsoft.com>
2013-10-22 14:39:41 -0700

    Initial commit

---
+++ Mvc5Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5Application.Startup))]
+namespace Mvc5Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
